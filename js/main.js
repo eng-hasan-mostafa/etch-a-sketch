8a146335b90fad4ba096125a5beca65db5dc487d
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-04-16 22:28:41 +0300

    Add progressive darkening effect when drawing
    Create the increaseSquareDarkens function to increase the darkness
    of the target cell by 10% with each effect.
    
    Also, create the randomizeSquareColor function to clean the code
    and increase readability.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 
+function generateRandomColor() {
+    //generate random number between 0 and 255
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
+    return `rgb(${red},${green},${blue})`;
+}
+
+function randomizeSquareColor(target) {
+    let randomColor = generateRandomColor();
+    target.style.backgroundColor = randomColor;
+    target.style.borderColor = randomColor;
+}
+
+function increaseSquareDarkens(target) {
+    let currentOpacity = target.style.opacity;
+    if(currentOpacity === 0) {
+        target.style.opacity = 0.1;
+    } else {
+        currentOpacity = Number(currentOpacity) +  0.1
+        target.style.opacity = currentOpacity;
+    }
+}
+
 function generateSketchpad(size) {
     const sketchpad = document.querySelector('.sketchpad');
     sketchpad.innerHTML = '';
@@ -10,10 +34,9 @@ function generateSketchpad(size) {
     } 
     //utilize event delegation to avoid performance issues. 
     sketchpad.addEventListener('mouseover', (e)=>{
-        let randomColor = generateRandomColor();
         let target = e.target.closest('.cell');
-        target.style.backgroundColor = randomColor;
-        target.style.borderColor = randomColor;
+        randomizeSquareColor(target);
+        increaseSquareDarkens(target);
     });
 }
 
@@ -28,11 +51,3 @@ resetButton.addEventListener('click', ()=>{
         generateSketchpad(userInput);
     }
 })
-
-function generateRandomColor() {
-    //generate random number between 0 and 255
-    let red = Math.floor(Math.random()*256);
-    let green = Math.floor(Math.random()*256);
-    let blue = Math.floor(Math.random()*256);
-    return `rgb(${red},${green},${blue})`;
-}
